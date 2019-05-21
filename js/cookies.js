3f66358fe37f11da84a5ea2d82da4e684f56ac03
root <root@android-h78o65m99e14p89s>
2019-05-21 13:54:27 -0400

    cookies

--- js/cookies.js
+++ js/cookies.js
@@ -1,10 +1,39 @@
-    
-    <script type="text/javascript">
-    function controlcookies() {
-             // si variable no existe se crea (al clicar en Aceptar)
-        localStorage.controlcookie = (localStorage.controlcookie || 0);
-     
-        localStorage.controlcookie++; // incrementamos cuenta de la cookie
-        cookie1.style.display='none'; // Esconde la política de cookies
+function GetCookie(name) {
+    var arg=name+"=";
+    var alen=arg.length;
+    var clen=document.cookie.length;
+    var i=0;
+ 
+    while (i<clen) {
+        var j=i+alen;
+ 
+        if (document.cookie.substring(i,j)==arg)
+            return "1";
+        i=document.cookie.indexOf(" ",i)+1;
+        if (i==0)
+            break;
     }
-    </script>
+ 
+    return null;
+}
+ 
+function aceptar_cookies(){
+    var expire=new Date();
+    expire=new Date(expire.getTime()+7776000000);
+    document.cookie="cookies_surestao=aceptada; expires="+expire;
+ 
+    var visit=GetCookie("cookies_surestao");
+ 
+    if (visit==1){
+        popbox3();
+    }
+}
+ 
+$(function() {
+    var visit=GetCookie("cookies_surestao");
+    if (visit==1){ popbox3(); }
+});
+ 
+function popbox3() {
+    $('#overbox3').toggle();
+}
